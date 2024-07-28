8fdfb901c5b8be454cf19808f6e842c57ac7ff08
Hoskar Jubeleo <hoskajubeleo@gmail.com>
2024-07-28 18:30:34 -0300

    chore: add shadcn theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "./_components/ui/button";
 
 export default function Home() {
   return (
-    <div className="w-full flex justify-center items-center">
+    <div className="w-full flex justify-center items-center flex-col">
       <h1>FSW - BARBER</h1>
       <Button>Test</Button>
     </div>
