9e7e3c1fbeb0412fd05c5f88ac0231c590554a3d
Hoskar Jubeleo <hoskajubeleo@gmail.com>
2024-07-28 18:28:34 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { Button } from "./_components/ui/button";
 
 export default function Home() {
   return (
-   <></>
+    <div className="w-full flex justify-center items-center">
+      <h1>FSW - BARBER</h1>
+      <Button>Test</Button>
+    </div>
   );
 }
