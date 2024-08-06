083ee012fd3b5a30a883097bc4389d86e07e7a96
Hoskar Jubeleo <hoskajubeleo@gmail.com>
2024-08-06 15:09:11 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
-import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="w-full flex justify-center items-center flex-col">
-      <h1>FSW - BARBER</h1>
-      <Button>Test</Button>
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Jamal190</h2>
+        <p>Segunda-feira, 05 de Agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Pesquisar" />
+          <Button size="icon">
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
+
+export default Home
