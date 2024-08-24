4ad129d69b7b4a1de6ad6ba9a81bbacec0974d1e
Hoskar Jubeleo <hoskajubeleo@gmail.com>
2024-08-23 22:04:16 -0300

    feat: add BookingSummary component

--- app/_components/booking-summary.tsx
+++ app/_components/booking-summary.tsx
@@ -1,47 +1,53 @@
-// TODO: terminar de crair componenete
+import { format } from "date-fns"
+import { Card, CardContent } from "./ui/card"
+import { Barbershop, BarbershopService } from "@prisma/client"
+import { ptBR } from "date-fns/locale"
 
-// import { format } from "date-fns"
-// import { Card, CardContent } from "./ui/card"
+interface BookingSummaryProps {
+  service: Pick<BarbershopService, "name" | "price">
+  barbershop: Pick<Barbershop, "name">
+  selectedDate: Date
+}
 
-// interface BookingSummaryProps {
+const BookingSummary = ({
+  service,
+  barbershop,
+  selectedDate,
+}: BookingSummaryProps) => {
+  return (
+    <Card>
+      <CardContent className="space-y-3 p-3">
+        <div className="flex items-center justify-between">
+          <h2 className="font-bold">{service.name}</h2>
+          <p className="text-sm font-bold">
+            {Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(Number(service.price))}
+          </p>
+        </div>
 
-// }
+        <div className="flex items-center justify-between">
+          <h2 className="text-sm text-gray-400">Data</h2>
+          <p className="text-sm">
+            {format(selectedDate, "d 'de' MMMM", {
+              locale: ptBR,
+            })}
+          </p>
+        </div>
 
-// const BookingSummary = ({  }: BookingSummaryProps) => {
-//   return (
-//     <Card>
-//       <CardContent className="space-y-3 p-3">
-//         <div className="flex items-center justify-between">
-//           <h2 className="font-bold">{service.name}</h2>
-//           <p className="text-sm font-bold">
-//             {Intl.NumberFormat("pt-BR", {
-//               style: "currency",
-//               currency: "BRL",
-//             }).format(Number(service.price))}
-//           </p>
-//         </div>
+        <div className="flex items-center justify-between">
+          <h2 className="text-sm text-gray-400">Hórario</h2>
+          <p className="text-sm">{format(selectedDate, "HH:mm")}</p>
+        </div>
 
-//         <div className="flex items-center justify-between">
-//           <h2 className="text-sm text-gray-400">Data</h2>
-//           <p className="text-sm">
-//             {format(selectedDay, "d 'de' MMMM", {
-//               locale: ptBR,
-//             })}
-//           </p>
-//         </div>
+        <div className="flex items-center justify-between">
+          <h2 className="text-sm text-gray-400">Barbearia</h2>
+          <p className="text-sm">{barbershop.name}</p>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
 
-//         <div className="flex items-center justify-between">
-//           <h2 className="text-sm text-gray-400">Hórario</h2>
-//           <p className="text-sm">{selectedTime}</p>
-//         </div>
-
-//         <div className="flex items-center justify-between">
-//           <h2 className="text-sm text-gray-400">Barbearia</h2>
-//           <p className="text-sm">{barbershop.name}</p>
-//         </div>
-//       </CardContent>
-//     </Card>
-//   )
-// }
-
-// export default BookingSummary
+export default BookingSummary
